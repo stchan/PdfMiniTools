9467e0df1788099b094682a2192583d50d4e2773
Sherman Chan <sherman.chan@pobox.com>
2012-01-05 11:15:05 -0500

    initial commit

---
+++ PdfMiniToolsCore/Core.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using iTextSharpText = iTextSharp.text;
+using iTextSharpPDF = iTextSharp.text.pdf;
+
+namespace PdfMiniToolsCore
+{
+    
+    public static class Core
+    {
+        #region Constants
+
+        private static const String exceptionArgumentNullOrEmptyString = "Parameter cannot be null, an empty string, or all whitespace.";
+
+        #endregion
+
+        public static Dictionary<String, String> RetrieveBasicProperties(String filename)
+        {
+            Dictionary<String, String> basicProperties = new Dictionary<string, string>();
+            if (!String.IsNullOrEmpty(filename) && !String.IsNullOrWhiteSpace(filename))
+            {
+                iTextSharpPDF.PdfReader documentReader = new iTextSharpPDF.PdfReader(new iTextSharpPDF.RandomAccessFileOrArray(filename), null);
+                basicProperties.Add("Page Count", documentReader.NumberOfPages.ToString());
+                basicProperties.Add("Encrypted", documentReader.IsEncrypted().ToString());
+                basicProperties.Add("Pdf Version", documentReader.PdfVersion.ToString());
+                documentReader.Close();
+            }
+            else
+            {
+                throw new ArgumentNullException("filename", exceptionArgumentNullOrEmptyString);
+            }
+            return basicProperties;
+        }
+    }
+}
---
+++ common/SharedAssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("Black Telescope Workshop")]
+[assembly: AssemblyProduct("PdfMiniTools")]
+[assembly: AssemblyCopyright("Copyright © Sherman Chan 2012")]
+[assembly: AssemblyTrademark("")]
+
+
+
+[assembly: AssemblyVersion("1.0.0.*")]
